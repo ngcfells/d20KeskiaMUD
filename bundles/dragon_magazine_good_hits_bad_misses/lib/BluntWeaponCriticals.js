db6eabb333603353167ae0c5a7b551ff73c30c9e
ngcfells <87544774+ngcfells@users.noreply.github.com>
2026-03-09 12:43:57 -0600

    Create BluntWeaponCriticals.js

---
+++ bundles/dragon_magazine_good_hits_bad_misses/lib/BluntWeaponCriticals.js
@@ -0,0 +1,23 @@
+01-31 double damage
+32-64 triple damage
+65-66 shield broken (no effect if no shield)
+67-68 shield broken (roll again if no shield)
+69-70 shield arm struck (no shield defense for 1-6 rounds)
+71-72 shield arm broken; lose shield
+73-74 weapon arm struck; hit probability -2
+75-76 weapon arm struck; hit probability -4
+77-78 weapon arm broken; no attacks
+79-80 hand struck; dexterity down 1-5 points until healed
+81-82 hand struck; dexterity down 1-5 points
+83-84 chest struck; stunned 1-6 rounds
+85-86 chest struck; ribs broken, lungs punctured; no movement
+87-88 chest struck; ribs broken, heart punctured; death.
+89-90 leg struck; fall to ground
+91-92 leg struck; movement halved
+93-94 leg broken; no movement
+95 head struck; lose 1-6 pts intelligence (no effect if helmed)
+96 head struck; lose 1-6 pts intelligence
+97 head struck; lose 2-12 pts intelligence (no effect if helmed)
+98 head struck; lose 2-12 pts intelligence
+99 skull crushed; dead (no effect if helmed)
+100 skull crushed; dead.

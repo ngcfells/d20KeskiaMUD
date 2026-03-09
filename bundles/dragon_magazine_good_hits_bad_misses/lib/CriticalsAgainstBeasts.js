ad2944bb53e0deae521fe243f009a35ddbddfe9d
ngcfells <87544774+ngcfells@users.noreply.github.com>
2026-03-09 12:50:03 -0600

    Create CriticalsAgainstBeasts.js

---
+++ bundles/dragon_magazine_good_hits_bad_misses/lib/CriticalsAgainstBeasts.js
@@ -0,0 +1,21 @@
+01-29 double damage
+30-58 triple damage
+59-60 limb removed at body; speed halved
+61-62 limb removed at midpoint; speed halved
+63-64 limb removed at midpoint; hit probability -2
+65-66 limb removed at body; hit probability -4
+67-68 limb removed at midpoint; speed halved, hit prob. -2
+69-70 limb removed at body; speed halved, hit probability -4
+71-72 throat cut: immediate death
+73-74 decapitated
+75-76 abdominal injuries; immediate death
+77-78 abdominal injuries; death in 2-12 turns
+79-80 abdominal injuries; death in 1-6 days
+81-82 chest injuries; immediate death
+83-84 chest injuries; death in 2-8 turns
+85-86 chest injuries; death in 1-4 days
+87-90 snout struck; immediate retreat
+91-94 snout struck; hit probability -2 for 1-4 rounds
+95-98 snout struck; hit probability -4 for 1-4 rounds
+99 head struck; stunned for 1-6 rounds
+100 head_struck: immediate death

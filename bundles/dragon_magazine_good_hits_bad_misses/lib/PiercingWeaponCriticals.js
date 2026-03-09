c278a5caa44f50b1b07acfde316934faafb723e5
ngcfells <87544774+ngcfells@users.noreply.github.com>
2026-03-09 12:46:58 -0600

    Create PiercingWeaponCriticals.js

---
+++ bundles/dragon_magazine_good_hits_bad_misses/lib/PiercingWeaponCriticals.js
@@ -0,0 +1,21 @@
+01-34 double damage
+35-70 triple damage
+71-72 shield arm struck (no effect if shield)
+73-74 shield arm struck
+75-76 weapon arm struck; hit probability -2
+77-78 weapon arm struck; hit probability -4
+79-80 weapon arm struck; no attacks
+81-82 struck in abdomen; death in 1-6 days
+83-84 struck in chest; death in 1-4 days
+85-86 struck in abdomen; death in 2-12 turns
+87-88 struck in chest; death in 2-8 turns
+89-90 struck in abdomen; immediate death
+91-92 struck in chest; immediate death
+93 blinded in eye (no effect if helmed)
+94 blinded in eye
+95 larynx punctured (no effect if helmed)
+96 larynx punctured
+97 struck in head; lose 1-6 pts intelligence (no effect if helmed)
+98 struck in head; lose 1-6 pts intelligence
+99 struck in head; immediate death (no effect if helmed)
+00 struck in head; immediate death
